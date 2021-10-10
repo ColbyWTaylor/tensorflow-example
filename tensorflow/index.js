b35e193e95e5794f67fc2e8070eebbce55ef2713
ColbyWTaylor <colbyt.093@gmail.com>
2021-10-10 01:50:18 +0000

    cloned tfjs - initial

---
+++ tensorflow/index.js
@@ -0,0 +1,10 @@
+// build a sequential model
+const model = tf.sequential();
+
+model.add(
+    tf.layers.simpleRNN({
+        inputShape: [20,4],
+        units: 20,
+        recurrentInitializer: 'GlorotNormal',
+    })
+)
